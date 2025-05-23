0200b6e1d236349c73082d91fec0c98b1177c32e
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2025-05-23 11:04:36 +0530

    update project

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -1,7 +1,35 @@
 package lk.ijse;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage stage)  throws Exception {
+
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/login.fxml"))));
+        stage.setTitle("Login");
+        stage.show();
+
+
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/server_form.fxml"))));
+        stage.setTitle("Server Form");
+        stage.centerOnScreen();
+        stage.show();
+
+        Stage secondstage = new Stage();
+        secondstage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/client_form.fxml"))));
+        secondstage.setTitle("Client Form");
+
+        secondstage.show();
+
+    }
+
+
 }
--- src/main/java/lk/ijse/controller/LoginController.java
+++ src/main/java/lk/ijse/controller/LoginController.java
@@ -1,4 +1,21 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
 public class LoginController {
+
+    @FXML
+    private Button loginbtn;
+
+    @FXML
+    private PasswordField loginpass;
+
+    @FXML
+    void loginbtnOnAction(ActionEvent event) {
+
+    }
+
 }
